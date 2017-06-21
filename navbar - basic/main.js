fca53cb7e069c268e319c53b74ef6c824720218e
samsm929 <sammiller19@roslynschools.org>
2017-06-21 07:41:29 -0400

    Add files via upload

---
+++ navbar - basic/main.js
@@ -0,0 +1,14 @@
+function openHome() {
+    $('.pages').show();
+    $('.pages').not("#home").hide();
+}
+
+function openPage1() {
+    $('.pages').show();
+    $('.pages').not("#page1").hide();
+}
+
+function openPage2() {
+    $('.pages').show();
+    $('.pages').not("#page2").hide();
+}
